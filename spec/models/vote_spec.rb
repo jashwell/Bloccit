426df424ba247d3463c4e96408b1e192b84a26b2
Jocelyn Ashwell <jocelynashwell@gmail.com>
2014-12-03 19:35:06 -0500

    Vote update checkpoint

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,3 +1,5 @@
+require 'rails_helper'
+
 describe Vote do
 
   describe "validations" do
@@ -5,13 +7,13 @@ describe Vote do
     describe "value validation" do
       it "only allows -1 or 1 as values" do
         v = Vote.new(value: -1)
-        expect( v.valid? ).to eq(true)
+        expect(v.valid?).to eq(true)
 
         v2 = Vote.new(value: 1)
-        expect( v.valid? ).to eq(true)
+        expect(v.valid?).to eq(true)
 
         v3 = Vote.new(value: 2)
-        expect( v.valid? ).to eq(false)
+        expect(v.valid?).to eq(false)
       end
 
 
