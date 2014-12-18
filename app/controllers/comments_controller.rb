4ecf1fd6af8b9d801dde44672bd54241e6267aea
Jocelyn Ashwell <jocelynashwell@gmail.com>
2014-12-17 20:04:15 -0500

    Ajax Create

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,31 +1,44 @@
 class CommentsController < ApplicationController
-  respond_to :html, :js
+  respond_to :js, :html
   def create
     @post = Post.find(params[:post_id])
-    @comment = @post.comments.new(params.require(:comment).permit(:body))
-    @comment.user_id = current_user.id
-    
-    if @comment.save
-      redirect_to [@post.topic, @post], notice: "Comment was saved successfully."
-    else
-      redirect_to [@post.topic, @post], notice: "Error saving comment."
-    end
-  end
+    @comments = @post.comments
 
-   def destroy
-    @post = Post.find(params[:post_id])
-    @comment = @post.comments.find(params[:id])
+    @comment = current_user.comments.build( comment_params )
+    @comment.post = @post
+    @new_comment = Comment.new
+
     authorize @comment
 
-    if @comment.destroy
-      flash[:notice] = "Comment was deleted."
+    if @comment.save
+      flash[:notice] = "Comment was created."
     else
-      flash[:error] = "Comment couldn't be deleted."
+      flash[:error] = "There was an error saving the comment. Please try again."
     end
 
     respond_with(@comment) do |format|
       format.html { redirect_to [@post.topic, @post] }
     end
   end
-end
 
+   def destroy
+     @post = Post.find(params[:post_id])
+     @comment = @post.comments.find(params[:id])
+     authorize @comment
+
+     if @comment.destroy
+       flash[:notice] = "Comment was deleted."
+     else
+       flash[:error] = "Comment couldn't be deleted."
+     end
+    respond_with(@comment) do |format|
+      format.html { redirect_to [@post.topic, @post] }
+    end
+   end
+
+     private
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
+end
